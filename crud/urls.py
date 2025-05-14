ff2d55e993facd80c5c0fa47dfef2bb1b2e230d6
joshuaquitoy22@gmail.com <joshuaquits@gmail.com>
2025-05-14 21:29:50 +0800

    edited views, urls, and hrefs

--- crud/urls.py
+++ crud/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('gender/add', views.addGender),
-    path('gender/list', views.genderList),
-    path('user/addUser', views.addUser),
-    path('gender/editGender/<int:genderId>', views.editGender),
-    path('gender/deleteGender/<int:genderId>', views.deleteGender),
-    path('user/userlist', views.userList),
+	path('genders/list', views.gender_list),
+	path('genders/add', views.add_gender),
+	path('genders/edit/<int:genderId>', views.edit_gender),
+	path('genders/delete/<int:genderId>', views.delete_gender),
 	path('users/add', views.add_user),
+	path('users/list', views.user_list),
 	path('users/edit/<int:userId>', views.edit_user),
 	path('users/delete/<int:userId>', views.delete_user),
-    
+	path('users/profile/<int:userId>', views.user_profile),
+
 ]
